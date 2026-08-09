6db2445a71beaca71761e74202c10e50e6bba056
sandman <ssn.jnk@gmail.com>
2026-08-09 08:22:07 -0400

    Release AJNanda 1.2.6

--- single.php
+++ single.php
@@ -142,12 +142,16 @@ get_header(); ?>
             </div>
         </article>
 
-        <?php
-        the_post_navigation(array(
-            'prev_text' => '<span class="nav-subtitle">Previous:</span> <span class="nav-title">%title</span>',
-            'next_text' => '<span class="nav-subtitle">Next:</span> <span class="nav-title">%title</span>',
-        ));
+        <div class="container">
+            <?php
+            the_post_navigation(array(
+                'prev_text' => '<span class="nav-subtitle">Previous:</span> <span class="nav-title">%title</span>',
+                'next_text' => '<span class="nav-subtitle">Next:</span> <span class="nav-title">%title</span>',
+            ));
+            ?>
+        </div>
 
+        <?php
         if (comments_open() || get_comments_number()) :
             comments_template();
         endif;
